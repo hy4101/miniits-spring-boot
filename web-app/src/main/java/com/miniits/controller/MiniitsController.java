41461297471d0b289000dc5a02cf9f14639ed210
wq <857454370@qq.com>
2017-10-07 20:14:22 +0800

    as

--- web-app/src/main/java/com/miniits/controller/MiniitsController.java
+++ web-app/src/main/java/com/miniits/controller/MiniitsController.java
@@ -1,22 +1,22 @@
-    package com.miniits.controller;
+package com.miniits.controller;
 
-    import org.springframework.stereotype.Controller;
-    import org.springframework.web.bind.annotation.RequestMapping;
-    import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-    /**
-     * @author: miniits
-     * @Date:
-     * @Time: <p>
-     * Description:
-     */
-    @Controller
-    @RequestMapping("/miniits")
-    public class MiniitsController {
+/**
+ * @author: miniits
+ * @Date:
+ * @Time: <p>
+ * Description:
+ */
+@Controller
+@RequestMapping("/miniits")
+public class MiniitsController {
 
-        @RequestMapping("index.html")
-        @ResponseBody
-        public String index(){
-            return "你好！迷你科技";
-        }
+    @RequestMapping("index.html")
+    @ResponseBody
+    public String index() {
+        return "你好！迷你科技";
     }
+}
